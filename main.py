cb951e18af5f79f5b705342f6bd74cefe8eced8f
Icastresana <114430336+Icastresana@users.noreply.github.com>
2023-11-02 23:41:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from torpy.http.requests import TorRequests
 import requests
 
-
 def scraper():
     lista = ""
     print('scraper : INFO : requesting elplan...', flush=True)
@@ -13,9 +12,9 @@ def scraper():
             with tor_requests.get_session() as sess:
                 grab = sess.get('https://hackmd.io/@algamo/DELANTERO-PICHICHI')
                 print(grab)
-    except:
-        print("scraper : INFO : torpy linea 22 could not access elcano")
-        sys.exit(0)
+    except Exception as e:
+        print(f"scraper : INFO : torpy linea 22 could not access elcano ({e})")
+        return  # Return instead of exiting the program
 
     soup = BeautifulSoup(grab.text, 'html.parser')
     for enlace in soup.find_all('a'):
@@ -27,23 +26,18 @@ def scraper():
         else:
             link = str(acelink).replace("acestream://", "")
             lista += str((canal + "\n" + link + "\n"))
-            contenido = ((lista.replace(u'\xa0', u' ')).strip())
-
+    
+    contenido = ((lista.replace(u'\xa0', u' ')).strip())
+    
     if contenido != "":
         print("scraper : OK : channels retrieved")
         write_cache(contenido)
     else:
         print("scraper : INFO : could not access elcano")
-    
 
 def write_cache(contenido):
-
     with open("cachedlist.txt", "wb") as cachedlist:
         cachedlist.write(contenido.encode('latin1'))
-        cachedlist.close()
         print("scraper : INFO : elcano cached")
-
-
-
 
 scraper()
