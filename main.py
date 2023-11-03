958cc467b98497b169a632c9e1a62c3b644c5454
Icastresana <114430336+Icastresana@users.noreply.github.com>
2023-11-04 00:10:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import re
+import re
+
+
 
 def scraper():
     url = 'https://hackmd.io/@algamo/DELANTERO-PICHICHI'
@@ -10,12 +13,31 @@ def scraper():
         print("Response content:")
         print(response.text)
 
-        lista = ""
-        matches = re.findall(r'\*\*(.*?)\*\*\(acestream://(.*?)\)', response.text)
-        for match in matches:
-            canal = match[0].strip()
-            acelink = match[1].strip()
-            lista += f" {canal}: acestream://{acelink}\n"
+       # ...
+
+    matches = re.findall(r'\*\*(.*?)\*\*\[:arrow_forward:\]\(acestream://(.*?)\s*(?:/|\\n|\\r\n|\s+)*', response.text)
+    canal_actual = ""
+    lista = ""
+
+    for match in matches:
+    canal = match[0].strip()
+    acelink = match[1].strip()
+    
+        if canal == canal_actual:
+        # El canal actual tiene múltiples enlaces, así que los agregamos
+        lista += f"acestream://{acelink}\n"
+        else:
+        # Cambio de canal, guardamos el canal anterior y sus enlaces
+        if canal_actual:
+            lista += f"{canal_actual}:\n{lista}\n"
+        canal_actual = canal
+        lista = f"acestream://{acelink}\n"
+
+# Asegúrate de agregar el último canal y sus enlaces
+if canal_actual:
+    lista += f"{canal_actual}:\n{lista}\n"
+
+# ...
 
         contenido = ((lista.replace(u'\xa0', u' ')).strip())
 
