e4da77f0d7532687837e608d7f32576531b75061
Icastresana <114430336+Icastresana@users.noreply.github.com>
2023-11-03 23:32:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,32 @@ def scraper():
     try:
         response = requests.get(url)
         response.raise_for_status()
-        print("Response content:")
-        print(response.text)
 
         lista = ""
-        matches = re.findall(r'\*\*(.*?)\*\*\[:arrow_forward:\]\(acestream://(.*?)\)', response.text)
+        matches = re.findall(r'\*\*(.*?)\*\*[:arrow_forward:]\(acestream://(.*?)\)', response.text)
+        
+        # Inicializar variables para el canal actual
+        current_channel = ""
+        current_links = []
+
         for match in matches:
             canal = match[0].strip()
             acelink = match[1].strip()
-            lista += f"{canal}: acestream://{acelink}\n"
+            
+            # Si el canal actual es igual al canal anterior, agregar el enlace
+            if canal == current_channel:
+                current_links.append(acelink)
+            else:
+                # Si el canal cambió, agregar todos los enlaces del canal anterior
+                if current_channel:
+                    lista += f"{current_channel}: acestream://{', '.join(current_links)}\n"
+                # Establecer el nuevo canal y reiniciar la lista de enlaces
+                current_channel = canal
+                current_links = [acelink]
+
+        # Agregar la última entrada después de salir del bucle
+        if current_channel:
+            lista += f"{current_channel}: acestream://{', '.join(current_links)}\n"
 
         contenido = ((lista.replace(u'\xa0', u' ')).strip())
 
@@ -32,6 +49,5 @@ def write_cache(contenido):
     with open("cachedlist.txt", "w", encoding='utf-8') as cachedlist:
         cachedlist.write(contenido)
         print("scraper : INFO : website data cached")
-
 
 scraper()
