7a9a20bdbe07d1e67e89218bf4a262cda7d74e2c
Icastresana <114430336+Icastresana@users.noreply.github.com>
2023-11-04 00:18:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import requests
 import re
 
-
-
 def scraper():
     url = 'https://hackmd.io/@algamo/DELANTERO-PICHICHI'
 
@@ -12,30 +10,27 @@ def scraper():
         print("Response content:")
         print(response.text)
 
-       # ...
-    matches = re.findall(r'\*\*(.*?)\*\*\[:arrow_forward:\]\(acestream://(.*?)(?:\s*[/\n\r\s]+|$)', response.text)
-    canal_actual = ""
-    lista = ""
+        matches = re.findall(r'\*\*(.*?)\*\*\[:arrow_forward:\]\(acestream://(.*?)(?:\s*[/\n\r\s]+|$)', response.text)
+        canal_actual = ""
+        lista = ""
 
-    for match in matches:
-    canal = match[0].strip()
-    acelink = match[1].strip()
+        for match in matches:
+            canal = match[0].strip()
+            acelink = match[1].strip()
     
-        if canal == canal_actual:
-        # El canal actual tiene múltiples enlaces, así que los agregamos
-        lista += f"acestream://{acelink}\n"
-        else:
-        # Cambio de canal, guardamos el canal anterior y sus enlaces
+            if canal == canal_actual:
+                # El canal actual tiene múltiples enlaces, así que los agregamos
+                lista += f"acestream://{acelink}\n"
+            else:
+                # Cambio de canal, guardamos el canal anterior y sus enlaces
+                if canal_actual:
+                    lista += f"{canal_actual}:\n{lista}\n"
+                canal_actual = canal
+                lista = f"acestream://{acelink}\n"
+
+        # Asegúrate de agregar el último canal y sus enlaces
         if canal_actual:
             lista += f"{canal_actual}:\n{lista}\n"
-        canal_actual = canal
-        lista = f"acestream://{acelink}\n"
-
-# Asegúrate de agregar el último canal y sus enlaces
-if canal_actual:
-    lista += f"{canal_actual}:\n{lista}\n"
-
-# ...
 
         contenido = ((lista.replace(u'\xa0', u' ')).strip())
 
