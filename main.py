acd01386e31ce22ca5ac44d132200d7fa6aa11d5
Icastresana <114430336+Icastresana@users.noreply.github.com>
2023-11-03 11:22:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 import requests
-from bs4 import BeautifulSoup
 import re
 
 def scraper():
     url = 'https://hackmd.io/@algamo/DELANTERO-PICHICHI'
-    # Definir los encabezados y las cookies
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
-        "Referer": "https://hackmd.io",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-    }
-
-    cookies = {
-        "locale": "es",
-        "connect.sid": "s%3AYutx3Z8hIKU7HkY8ygAsTEVHaNawfF0j.18Kq%2F7CUS0cLCsplAY0zlFScsCmYlJUyQhXgMu0YeNg",
-        # Otras cookies que puedas necesitar
-    }
 
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Verificar si la respuesta tiene éxito (código de estado 200)
+        response.raise_for_status()
         print("Response content:")
         print(response.text)
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-
         lista = ""
-        for match in re.finditer(r'\*\*([^\*]+)\*\*\[NL\]\[:arrow_forward:\]\(acestream://[^\)]+\)', response.text):
-            canal = match.group(1)
-            acelink = match.group(0).split(":](acestream://")[1]
+        matches = re.findall(r'([^/]+)\[:arrow_forward:\]\(acestream://[^\)]+\)', response.text)
+        for match in matches:
+            canal = match[0].strip()
+            acelink = match[1].strip()
             lista += f"{canal}: {acelink}\n"
 
         contenido = ((lista.replace(u'\xa0', u' ')).strip())
