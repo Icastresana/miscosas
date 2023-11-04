7767ec7d7e3f18dc464b7e0d37b8efa5fe05450a
Icastresana <114430336+Icastresana@users.noreply.github.com>
2023-11-04 08:31:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,35 +7,41 @@ def scraper():
     try:
         response = requests.get(url)
         response.raise_for_status()
-        print("Response content:")
-        print(response.text)
 
         lista = ""
-        matches = re.findall(r'\*\*(.*?)\*\*(?::checkered_flag:)?\[:arrow_forward:\]\(acestream://(.*?)\)', response.text)
+        matches = re.finditer(r'\*\*(.*?)\*\*(?::checkered_flag:)?\[:arrow_forward:\]\(acestream://(.*?)\)', response.text)
 
+        canales = {}
         canal_actual = None
 
         for match in matches:
-            canal, enlace = match
-            if canal_actual is None or canal != canal_actual:
+            canal, enlace = match.groups()
+            
+            if canal != canal_actual:
+                if canal_actual:
+                    lista += "\n".join(canales[canal_actual]) + "\n"
                 canal_actual = canal
-                lista += canal + ":\n"
-            lista += enlace + "\n"
+                canales[canal] = [f"{canal}:\nacestream://{enlace}"]
+            else:
+                canales[canal].append(f"acestream://{enlace}")
+
+        if canal_actual:
+            lista += "\n".join(canales[canal_actual]) + "\n"
 
         contenido = ((lista.replace(u'\xa0', u' ')).strip())
 
-        if contenido != "":
-            print("scraper : OK : channels retrieved")
+        if contenido:
+            print("scraper: OK: channels retrieved")
             write_cache(contenido)
         else:
-            print("scraper : INFO : could not access the website")
+            print("scraper: INFO: could not access the website")
 
     except requests.exceptions.RequestException as e:
-        print(f"scraper : ERROR : {e}")
+        print(f"scraper: ERROR: {e}")
 
 def write_cache(contenido):
     with open("cachedlist.txt", "w", encoding='utf-8') as cachedlist:
         cachedlist.write(contenido)
-        print("scraper : INFO : website data cached")
+        print("scraper: INFO: website data cached")
 
 scraper()
