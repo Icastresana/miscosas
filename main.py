cefb66fe4d3fc2bc85ba023b8d3b6524722980ce
Icastresana <114430336+Icastresana@users.noreply.github.com>
2023-11-04 20:50:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def scraper():
             if not acelinks:
                 acelinks = [""]  # Agrega una cadena vacía si no se encontraron enlaces
             if "WINDOWS ACESTREAM" not in canal and "ANDROID ACESTREAM.APK" not in canal and "AQUÍ" not in canal and "EUR / RU / NA / SA - TV" not in canal:
-            for acelink in acelinks:
-                lista += f"{canal}:\nacestream://{acelink}\n"
+                for acelink in acelinks:
+                    lista += f"{canal}:\nacestream://{acelink}\n"
 
 
 
