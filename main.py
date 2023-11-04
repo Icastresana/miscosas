be0bfe5f7db9882d605bda96842ad77f72071796
Icastresana <114430336+Icastresana@users.noreply.github.com>
2023-11-04 08:52:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,48 +7,31 @@ def scraper():
     try:
         response = requests.get(url)
         response.raise_for_status()
+        print("Response content:")
+        print(response.text)
 
         lista = ""
-        matches = re.finditer(r'\*\*(.*?)\*\*\(acestream://(.*?)\)$', response.text)
-
-        canales = {}
-        canal_actual = None
-
+        matches = re.findall(r'\*\*(.*?)\*\*\[:arrow_forward:\]\(acestream://(.*?)\)', response.text)
         for match in matches:
-            canal, enlace = match.groups()
-
-            if canal_actual != canal:
-                if canal_actual:
-                    if canales[canal_actual]:
-                        lista += f"{canal_actual}\n" + "\n".join(canales[canal_actual]) + "\n"
-                    else:
-                        lista += f"{canal_actual}\n"
-                canal_actual = canal
-                canales[canal] = []
-
-            if enlace:
-                canales[canal].append(f"acestream://{enlace}")
-
-        if canal_actual:
-            if canales[canal_actual]:
-                lista += f"{canal_actual}\n" + "\n".join(canales[canal_actual]) + "\n"
-            else:
-                lista += f"{canal_actual}\n"
+            canal = match[0].strip()
+            acelink = match[1].strip()
+            lista += f"{canal}: acestream://{acelink}\n"
 
         contenido = ((lista.replace(u'\xa0', u' ')).strip())
 
-        if contenido:
-            print("scraper: OK: channels retrieved")
+        if contenido != "":
+            print("scraper : OK : channels retrieved")
             write_cache(contenido)
         else:
-            print("scraper: INFO: could not access the website")
+            print("scraper : INFO : could not access the website")
 
     except requests.exceptions.RequestException as e:
-        print(f"scraper: ERROR: {e}")
+        print(f"scraper : ERROR : {e}")
+
 
 def write_cache(contenido):
     with open("cachedlist.txt", "w", encoding='utf-8') as cachedlist:
         cachedlist.write(contenido)
-        print("scraper: INFO: website data cached")
+        print("scraper : INFO : website data cached")
 
 scraper()
