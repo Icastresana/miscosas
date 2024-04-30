7f950b1db91c7a316687a41c37b30b176c1d163f
Icastresana <114430336+Icastresana@users.noreply.github.com>
2024-04-30 18:38:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import re
 
 def scraper():
-    url = 'https://hackmd.io/@penaltis/YELLOW-CARD'
+    url = 'https://telegra.ph/EL-PLAN-DEPORTES-03-19'
 
     try:
         response = requests.get(url)
@@ -10,23 +10,14 @@ def scraper():
         print("Response content:")
         print(response.text)
 
+        # Patrón de expresión regular para eliminar caracteres no deseados del nombre del canal
+        patron = re.compile(r'^\W*')
         lista = ""
-        matches = re.findall(r'\*\*(.*?)\*\*([^*]+)', response.text)
+        matches = re.findall(r'<p>([^<]+)<\/p>\s*<p>(?:acestream:\/\/)?([0-9a-fA-F]+)<\/p>', response.text)
         for match in matches:
-            canal = match[0].strip()
-            acelinks = re.findall(r'\(acestream://(.*?)\)', match[1])
-            if not acelinks:
-                acelinks = [""]  # Agrega una cadena vacía si no se encontraron enlaces
-            if "WINDOWS ACESTREAM" not in canal and "ANDROID ACESTREAM.APK" not in canal and "AQUÍ" not in canal and "EUR / RU / NA / SA - TV" not in canal and "ESP - TV" not in canal:
-                if "720p" in canal:
-                    canal_anterior = re.sub(r'1080[p]?', '', canal_anterior)  # Elimina "1080p" o "1080" del nombre anterior
-                    canal_anterior = re.sub(r' MultiAudio', '',canal_anterior) # Elimina "multiaudio" del nombre anterior
-                    canal = canal_anterior + canal  # Agrega el nombre del canal anterior
-                canal_anterior = canal  # Actualiza el nombre del canal anterior
-                for acelink in acelinks:
-                    lista += f"{canal}:\nacestream://{acelink}\n"
-
-
+            canal = patron.sub('', match[0].strip())  # Limpiar el nombre del canal
+            acelinks = match[1]
+            lista += f"{canal}:\nacestream://{acelinks}\n"
 
 
         contenido = ((lista.replace(u'\xa0', u' ')).strip())
