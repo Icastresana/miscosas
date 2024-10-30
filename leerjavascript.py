ceb81101218f3968b50f12b31e8d43ee37925228
Icastresana <114430336+Icastresana@users.noreply.github.com>
2024-10-30 22:16:34 +0100

    Update leerjavascript.py

--- leerjavascript.py
+++ leerjavascript.py
@@ -1,17 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Modo sin interfaz
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-# Inicializa Chrome automáticamente usando WebDriver Manager
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-
-
 
 try:
     # Navegar a la URL
@@ -59,4 +45,3 @@ except Exception as e:
 finally:
     # Cerrar el navegador
     driver.quit()
-
