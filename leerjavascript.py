c8ecb60a120f5a9e634edaa7dc4523f8787f6faf
Icastresana <114430336+Icastresana@users.noreply.github.com>
2024-10-30 21:22:47 +0100

    Update leerjavascript.py

--- leerjavascript.py
+++ leerjavascript.py
@@ -1,17 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Configurar opciones de Chrome para modo headless
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Ejecutar Chrome en modo headless
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-# Inicializar el navegador con las opciones configuradas
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-
+# Inicializar el navegador
+driver = webdriver.Chrome()
 
 try:
     # Navegar a la URL
